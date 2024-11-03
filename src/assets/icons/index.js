16a1fcf332d78105bfd2c33d2b3760855a9cf3d5
Simone Baptiste <baptistesimone19@gmail.com>
2024-11-03 23:15:33 +0100

    removed unused images and models

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -5,23 +5,13 @@ import github from './github.svg'
 import html from './html.svg'
 import javascript from './javascript.svg'
 import mongodb from './mongodb.svg'
-import motion from './motion.svg'
-import mui from './mui.svg'
-import nextjs from './nextjs.svg'
 import nodejs from './nodejs.svg'
 import react from './react.svg'
-import redux from './redux.svg'
 import sass from './sass.svg'
 import tailwindcss from './tailwindcss.svg'
 import typescript from './typescript.svg'
 import linkedin from './linkedin.svg'
 import twitter from './twitter.svg'
-import car from './car.svg'
-import estate from './estate.svg'
-import pricewise from './pricewise.svg'
-import snapgram from './snapgram.svg'
-import summiz from './summiz.svg'
-import threads from './threads.svg'
 import arrow from './arrow.svg'
 import contact from './contact.svg'
 import soundon from './soundon.png'
@@ -43,27 +33,17 @@ export {
     html,
     javascript,
     mongodb,
-    motion,
-    mui,
     godot,
     go,
-    nextjs,
     nodejs,
     react,
-    redux,
     sass,
     java,
     tailwindcss,
     typescript,
     linkedin,
     twitter,
-    car,
-    estate,
-    pricewise,
     angular,
-    snapgram,
-    summiz,
-    threads,
     cortis,
     unity,
     arrow,
