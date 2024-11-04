81aa7f59e0b6a222a1b60bf75b3a7762faa40e58
Simone Baptiste <baptistesimone19@gmail.com>
2024-11-05 00:00:23 +0100

    type checks

--- src/models/Airplane.tsx
+++ src/models/Airplane.tsx
@@ -2,10 +2,10 @@ import {useAnimations, useGLTF} from "@react-three/drei";
 // @ts-ignore
 import airplaneScene from '../assets/3d/aereo2.glb';
 import {useEffect, useRef} from "react";
+import {BufferGeometry, Mesh, NormalBufferAttributes} from "three";
 
-// @ts-ignore
-const Airplane = ({isRotating, ...props}) => {
-    const ref = useRef();
+const Airplane = ({isRotating, ...props}: {isRotating: boolean, props: any}) => {
+    const ref = useRef<Mesh<BufferGeometry<NormalBufferAttributes>>>(null);
     const {scene, animations} = useGLTF(airplaneScene);
     const {actions} = useAnimations(animations, ref);
 
